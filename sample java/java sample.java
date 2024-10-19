93baa5bd37585d012a6eaa1eddaec4c1fdc9bd0c
venkateswarlu51 <thirumalasettyvenkateswarlu57@gmail.com>
2024-10-19 16:14:42 +0530

    Update java sample.java

--- sample java/java sample.java
+++ sample java/java sample.java
@@ -2,6 +2,6 @@ public class Hello
 {
       public static void main(String[] args)
       {
-             System.out,print("hiiiiiiiiiiiiiiiiiiiiii");
+             System.out.print("hiiiiiiiiiiiiiiiiiiiiii");
        }
 }
